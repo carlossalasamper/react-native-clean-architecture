9d679fab91d207e8dd8d9f8812b88627fc65465a
Nguyễn Nhật Long <longnn22@viettel.com.vn>
2024-05-09 14:10:04 +0700

    chore: Update import statements to use single quotes consistently

--- src/post/domain/specifications/IPostRepository.ts
+++ src/post/domain/specifications/IPostRepository.ts
@@ -1,8 +1,8 @@
-import PostEntity from "../entities/PostEntity";
-import GetPostsPayload from "../../application/types/GetPostsPayload";
-import GetPostsResponse from "src/post/application/types/GetPostsResponse";
+import PostEntity from '../entities/PostEntity';
+import GetPostsPayload from '../../application/types/GetPostsPayload';
+import GetPostsResponse from 'src/post/application/types/GetPostsResponse';
 
-export const IPostRepositoryToken = Symbol("IPostRepository");
+export const IPostRepositoryToken = Symbol('IPostRepository');
 
 export interface IPostRepository {
   find: (id: number) => Promise<PostEntity>;
