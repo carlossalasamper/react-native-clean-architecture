197993b1dd8475e434b5130c260aad5a3491f7c5
duy <duydq2k1@gmail.com>
2024-05-18 18:34:09 +0700

    add screen write

--- src/core/presentation/navigation/RootNavigator.tsx
+++ src/core/presentation/navigation/RootNavigator.tsx
@@ -2,7 +2,8 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from './types';
 import PostScreen from 'src/post/presentation/screens/PostScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
-import PostsScreen from 'src/post/presentation/screens/PostsScreen';
+import TestScreen from 'src/post/presentation/screens/TestScreen';
+import LessonScreen from 'src/post/presentation/screens/LessonScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -10,11 +11,16 @@ export default function RootNavigator() {
   const initialRouteName: keyof RootStackParamList = 'NotFound';
 
   return (
-    <Stack.Navigator initialRouteName={initialRouteName}>
-      <Stack.Screen name="NotFound" component={NotFoundScreen} />
-      <Stack.Screen name="Posts" component={PostsScreen} />
+    <Stack.Navigator
+      initialRouteName={initialRouteName}
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Posts" component={LessonScreen} />
 
       <Stack.Screen name="Post" component={PostScreen} />
+
+      <Stack.Screen name="TestCanvas" component={TestScreen} />
+
+      <Stack.Screen name="NotFound" component={NotFoundScreen} />
     </Stack.Navigator>
   );
 }
