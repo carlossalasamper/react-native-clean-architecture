ba93a313cee56c29878046b17caeb7c335222039
Nguyễn Nhật Long <longnn22@viettel.com.vn>
2024-05-21 23:18:36 +0700

    chore: add lesson module

---
+++ src/lesson/PostModule.ts
@@ -0,0 +1,29 @@
+import {getModuleContainer, module} from 'inversify-sugar';
+import {GetPostsStore} from './presentation/stores/GetPostsStore/GetPostsStore';
+import {FindPostStore} from './presentation/stores/FindPostStore/FindPostStore';
+import {IPostRepositoryToken} from './domain/specifications/IPostRepository';
+import PostRepository from './infrastructure/implementations/PostRepository';
+import FindPostUseCase from './application/useCases/FindPostUseCase';
+import GetPostsUseCase from './application/useCases/GetPostsUseCase';
+
+@module({
+  providers: [
+    {
+      provide: IPostRepositoryToken,
+      useClass: PostRepository,
+    },
+    FindPostUseCase,
+    GetPostsUseCase,
+    {
+      useClass: GetPostsStore,
+      scope: 'Transient',
+    },
+    {
+      useClass: FindPostStore,
+      scope: 'Transient',
+    },
+  ],
+})
+export class PostModule {}
+
+export const postModuleContainer = getModuleContainer(PostModule);
